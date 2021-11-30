aa9977bf129759ed73974d8b3b805ab22185494d
kuangkuangkuangha <75719597+kuangkuangkuangha@users.noreply.github.com>
2021-11-30 21:01:28 +0800

    增加两个问卷答案

--- app/controller/UserController.go
+++ app/controller/UserController.go
@@ -13,7 +13,7 @@ import (
 
 // 生成6位随机用户名
 func SendUserName(c *gin.Context) {
-	userName := fmt.Sprintf("%06v", rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(1000000))
+	userName := fmt.Sprintf("%03v", rand.New(rand.NewSource(time.Now().UnixNano())).Int31n(1000))
 	c.JSON(http.StatusOK, helper.ApiReturn(1, "生成用户名成功", userName))
 }
 
@@ -29,6 +29,42 @@ func StoreUserInfo(c *gin.Context) {
 	}
 
 	res := userModel.StoreUserInfo(userJson)
+	if res.Status == -1 {
+		c.JSON(http.StatusNotFound, helper.ApiReturn(res.Status, res.Msg, res.Data))
+	}
+
+	c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
+}
+
+// 存入第一份问卷的答案
+func QuestionNaireOne(c *gin.Context) {
+	var answerJson model.UserInfoOne
+	answerModel := model.UserInfoOne{}
+
+	if err := c.ShouldBindJSON(&answerJson); err != nil {
+		c.JSON(http.StatusNotFound, helper.ApiReturn(-1, "数据模型绑定失败", err))
+		return
+	}
+
+	res := answerModel.QuestionNaireOne(answerJson)
+	if res.Status == -1 {
+		c.JSON(http.StatusNotFound, helper.ApiReturn(res.Status, res.Msg, res.Data))
+	}
+
+	c.JSON(http.StatusOK, helper.ApiReturn(res.Status, res.Msg, res.Data))
+}
+
+// 存入第二份问卷的答案
+func QuestionNaireTwo(c *gin.Context) {
+	var answerJson model.UserInfoTwo
+	answerModel := model.UserInfoTwo{}
+
+	if err := c.ShouldBindJSON(&answerJson); err != nil {
+		c.JSON(http.StatusNotFound, helper.ApiReturn(-1, "数据模型绑定失败", err))
+		return
+	}
+
+	res := answerModel.QuestionNaireTwo(answerJson)
 	if res.Status == -1 {
 		c.JSON(http.StatusNotFound, helper.ApiReturn(res.Status, res.Msg, res.Data))
 	}
--- app/model/UserModel.go
+++ app/model/UserModel.go
@@ -11,6 +11,26 @@ type User struct {
 	Time     string `json:"time" gorm:"time" binding:"required"`
 }
 
+type UserInfoOne struct {
+	ID       int    `json:"id" gorm:"id" `
+	UserName string `json:"user_name" gorm:"user_name" binding:"required"`
+	GroupId  int    `json:"group_id" gorm:"grop_id" binding:"required"`
+	Tel      string `json:"tel" gorm:"tel" binding:"required"`
+	Point    int    `json:"point" gorm:"point" binding:"required"`
+	Time     string `json:"time" gorm:"time" binding:"required"`
+	Answer   string `json:"answer" gorm:"answer" binding:"required"`
+}
+
+type UserInfoTwo struct {
+	ID       int    `json:"id" gorm:"id" `
+	UserName string `json:"user_name" gorm:"user_name" binding:"required"`
+	GroupId  int    `json:"group_id" gorm:"grop_id" binding:"required"`
+	Tel      string `json:"tel" gorm:"tel" binding:"required"`
+	Point    int    `json:"point" gorm:"point" binding:"required"`
+	Time     string `json:"time" gorm:"time" binding:"required"`
+	Answer   string `json:"answer" gorm:"answer" binding:"required"`
+}
+
 // 存储信息
 func (model *User) StoreUserInfo(data User) helper.ReturnType {
 	err := db.Table("heart").Create(&data).Error
@@ -18,5 +38,23 @@ func (model *User) StoreUserInfo(data User) helper.ReturnType {
 		return helper.ReturnRes(-1, "存储信息失败", err.Error())
 	} else {
 		return helper.ReturnType{Status: 1, Msg: "存储信息成功", Data: data}
+	}
+}
+
+func (model *UserInfoOne) QuestionNaireOne(data UserInfoOne) helper.ReturnType {
+	err := db.Table("q1").Create(&data).Error
+	if err != nil {
+		return helper.ReturnRes(-1, "存储问卷一信息失败", err.Error())
+	} else {
+		return helper.ReturnType{Status: 1, Msg: "存储问卷一成功", Data: data}
+	}
+}
+
+func (model *UserInfoTwo) QuestionNaireTwo(data UserInfoTwo) helper.ReturnType {
+	err := db.Table("q2").Create(&data).Error
+	if err != nil {
+		return helper.ReturnRes(-1, "存储问卷二信息失败", err.Error())
+	} else {
+		return helper.ReturnType{Status: 1, Msg: "存储问卷二成功", Data: data}
 	}
 }
